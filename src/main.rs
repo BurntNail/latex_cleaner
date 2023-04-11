c09b49b0023305f5bf228945778ccf16a7ffcafd
Jack Maguire <github.29x6f@simplelogin.com>
2023-04-11 15:16:15 +0100

    add support for recompiling LaTeX files

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,61 @@
-use std::{env, env::current_dir, ffi::OsStr, fs::remove_file, path::PathBuf};
+#![warn(clippy::all, clippy::nursery, clippy::pedantic)]
+
+use color_eyre::eyre::eyre;
+use std::{
+    env,
+    env::current_dir,
+    ffi::OsStr,
+    fs::remove_file,
+    path::PathBuf,
+    process::{Command, Stdio},
+};
 use walkdir::WalkDir;
 
-fn main() {
-    let target_extensions = [OsStr::new("aux"), OsStr::new("log"), OsStr::new("gz")];
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install().expect("unable to install color-eyre");
 
-    let path = env::args()
-        .nth(1)
-        .map(PathBuf::from)
-        .unwrap_or_else(|| current_dir().expect("unable to get current working directory."));
-    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+    let mut env_args = env::args().skip(1);
+
+    let target_remove_extensions = [OsStr::new("aux"), OsStr::new("log"), OsStr::new("gz")];
+    let target_compile_extensions = [OsStr::new("tex")];
+
+    let path = env_args
+        .next()
+        .map_or_else(current_dir, |p| Ok(PathBuf::from(p)))?;
+    let mut update_before = env_args
+        .next()
+        .map_or(false, |s| s.trim().eq_ignore_ascii_case("u"));
+
+    for entry in WalkDir::new(path).into_iter().filter_map(Result::ok) {
         let path = entry.path();
-        if path
-            .extension()
-            .map_or(false, |e| target_extensions.contains(&e))
-        {
-            match remove_file(path) {
-                Ok(_) => println!("Successfully removed {path:?}"),
-                Err(e) => eprintln!("Error removing {path:?}: {e:?}"),
+        if let Some(ext) = path.extension() {
+            if update_before && target_compile_extensions.contains(&ext) {
+                println!("Compiling {path:?}");
+
+                let parent = path.parent().ok_or(eyre!("unable to get path parent"))?;
+                let status = Command::new("pdflatex")
+                    .current_dir(parent)
+                    .arg("-interaction=nonstopmode")
+                    .arg(path)
+                    .stdout(Stdio::null())
+                    .status()?;
+
+                if status.success() {
+                    println!("Successfully compiled: {path:?}");
+                } else {
+                    eprintln!(
+                        "Failed to compile {path:?} - stopping future compilations: {status:?}."
+                    );
+                    update_before = false;
+                }
+            } else if target_remove_extensions.contains(&ext) {
+                match remove_file(path) {
+                    Ok(_) => println!("Successfully removed {path:?}"),
+                    Err(e) => eprintln!("Error removing {path:?}: {e:?}"),
+                }
             }
         }
     }
+
+    Ok(())
 }
