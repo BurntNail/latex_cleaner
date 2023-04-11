ece8d5b0136105322af398331e9346d5d4420a5c
Jack Maguire <github.29x6f@simplelogin.com>
2023-04-11 14:46:18 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::{env, env::current_dir, ffi::OsStr, fs::remove_file, path::PathBuf};
+use walkdir::WalkDir;
+
+fn main() {
+    let target_extensions = [
+        OsStr::new("aux"),
+        OsStr::new("log"),
+        OsStr::new("synctex.gz"),
+    ];
+
+    let path = env::args()
+        .nth(1)
+        .map(PathBuf::from)
+        .unwrap_or_else(|| current_dir().expect("unable to get current working directory."));
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+        let path = entry.path();
+        if path
+            .extension()
+            .map_or(false, |e| target_extensions.contains(&e))
+        {
+            match remove_file(path) {
+                Ok(_) => println!("Successfully removed {path:?}"),
+                Err(e) => eprintln!("Error removing {path:?}: {e:?}"),
+            }
+        }
+    }
+}
